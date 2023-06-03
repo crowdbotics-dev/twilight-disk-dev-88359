df4d085427ffb8d625d8f87cde85edc35175e975
Crowdbotics <engineering@crowdbotics.com>
2023-06-03 23:23:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/twilight_disk_dev_88359/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twilight_disk_dev_88359;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "twilight_disk_dev_88359";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-disk-dev-88359",
+  url: "https://twilight-disk-dev-88359.botics.co",
+  api: "https://twilight-disk-dev-88359.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
